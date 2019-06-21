cf51149aead64ad628d9a1294a8cf3626ed8d8eb
shawn <shawn@gordhamer.com>
2019-06-21 12:21:19 -0500

    constructor with named parameters

--- lib/components/ball.dart
+++ lib/components/ball.dart
@@ -4,41 +4,41 @@ import 'package:flame/components/component.dart';
 
 class Ball extends Component {
   // instance variables
+  double ballSize;  // radius of ball circle in pixels
+  double speedScale;  // how many times screen width the speed will be
   double x = 0;  // the x location of the ball
   double y = 0;  // the y location of the
-  double width;  // width of screen
-  double height;  // height of screen
-  double speedX;  // speed in the x direction
-  double speedY;  // speed in the y direction
-  double sizeX;  // size of the screen in the x direction
-  double sizeY;  // size of the screen in the y direction
+  double speedX=0;  // speed in the x direction
+  double speedY=0;  // speed in the y direction
+  double sizeX=0;  // size of the screen in the x direction
+  double sizeY=0;  // size of the screen in the y direction
   int lives = 10;  // how many bounces until the ball dies
   Paint paint = Paint();  // paint the ball circle
 
   // create a ball
-  Ball() : super() {
-    print("new Ball");
-    paint.color = Colors.green;
+  Ball({Color color=Colors.white, double size=10, double speed=1, PaintingStyle style = PaintingStyle.stroke}) : super() {
+    paint.color = color;
     paint.strokeWidth = 1;
-    paint.style = PaintingStyle.stroke;
+    paint.style = style;
+    ballSize = size;
+    speedScale = speed;
   }
 
   // the game engine will tell you what the screen size is
   void resize(Size size) {
-    print("ball.resize: size = $size");
     // save screen width and height
-    width = size.width;
-    height = size.height;
+    sizeX = size.width;
+    sizeY = size.height;
 
     // set the speed of the ball
-    // travel in the x and y direction at the speed of 1/5 screen width per second
-    speedX = width/5;
-    speedY = width/5;
+    // travel in the x and y direction at the speed of speedScale screen widths per second
+    speedX = sizeX*speedScale;
+    speedY = sizeX*speedScale;
   }
 
   // draw this component whenever the game engine tells you to
   void render(Canvas c) {
-    c.drawCircle(Offset(x,y), 10, paint);
+    c.drawCircle(Offset(x,y), ballSize, paint);
   }
 
   // update this component whenever the game engine tells you to
@@ -48,14 +48,14 @@ class Ball extends Component {
     y += t*speedY;
 
     // change direction if ball crosses edge
-    if (x < 0) {
-      speedX = -speedX;
-      x = 0;
-      lives--;
+    if (x < 0) {  // off the left edge
+      speedX = -speedX;  // reverse direction
+      x = 0;  // put it back at the left edge
+      lives--;  // a bounce loses a life
     }
-    if (x > width) {
+    if (x > sizeX) {
       speedX = -speedX;
-      x = width;
+      x = sizeX;
       lives--;
     }
     if (y < 0) {
@@ -63,9 +63,9 @@ class Ball extends Component {
       y = 0;
       lives--;
     }
-    if (y > height) {
+    if (y > sizeY) {
       speedY = -speedY;
-      y = height;
+      y = sizeY;
       lives--;
     }
 
@@ -74,12 +74,14 @@ class Ball extends Component {
 
   // tell the game engine if this component should be destroyed
   // whenever it asks
-  bool destroy() {
+/*  bool destroy() {
     if (lives > 0) {  // not dead yet
       return false;
     } else {  // dead
       return true;
     }
   }
+*/
+  bool destroy() => lives <= 0;
 
 }
--- lib/flutterball_game.dart
+++ lib/flutterball_game.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:flutter_ball/components/ball.dart';
 
@@ -6,7 +7,7 @@ class FlutterballGame extends BaseGame {
   // make a new game
   FlutterballGame() {
     // make a new ball game component
-    var ball = Ball();
+    var ball = Ball(color: Colors.blue, size: 20, speed: 0.5, style: PaintingStyle.fill);
 
     // tell the game about this component
     add(ball);
