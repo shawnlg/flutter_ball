2f143cb8268897d2d4d9eb13464a9650721bd69b
shawn <shawn@gordhamer.com>
2019-07-26 19:02:27 -0500

    update version, comments

--- lib/components/ball_releaser.dart
+++ lib/components/ball_releaser.dart
@@ -9,14 +9,13 @@ class BallReleaser extends Component {
 
   // instance variables
   FlutterballGame game;
-  int lives = 100;  // how many balls to release until this component quits
+  int lives = 10;  // how many balls to release until this component quits
   double timeOfNextBall;  // when to add another ball
-  Random rnd;  // rnandom number generator
+  Random rnd = Random();  // random number generator
 
   // create the component, passing in the game object
   BallReleaser(this.game) : super() {
     timeOfNextBall = game.currentTime() + 1;
-    rnd = Random();
   }
 
   void render(Canvas c) => null;
@@ -29,7 +28,7 @@ class BallReleaser extends Component {
     // make a new ball game component with some random values
     double speedX = rnd.nextDouble()*1.8 + 0.2;  // x speed between 0.2 and 2
     double speedY = rnd.nextDouble()*1.8 + 0.2;  // y speed between 0.2 and 2
-    double size = rnd.nextDouble()*16 + 5;  // size between 5 and 20
+    double size = rnd.nextDouble()*15 + 5;  // size between 5 and 20
     PaintingStyle style = rnd.nextBool() ? PaintingStyle.stroke : PaintingStyle.fill;
     Color color;
     switch (rnd.nextInt(6)) {
