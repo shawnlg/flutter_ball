d226c55455dfa4e46d25a6a9c89e962ac65a48c0
shawn <shawn@gordhamer.com>
2019-08-13 20:58:23 -0500

    add text component

---
+++ lib/components/text.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flame/components/component.dart';
+
+class TextDraw extends Component {
+  // instance variables
+  int lives = 1;  // used to destroy the component
+  Paint paint;  // paint the rectangle
+  Paint border;  // paint the border
+  Rect position;  // position of block
+
+  // show text inside box
+  TextSpan textSpan;  // used to paint text
+  TextPainter tp = TextPainter(
+    textDirection: TextDirection.ltr,
+  );
+
+  TextDraw(this.position, this.textSpan, {
+    Color boxColor, Color borderColor,
+    textAlign:TextAlign.center,
+    borderThickness:1.0, scale:2.0,
+  }) : super() {
+    if (boxColor != null) {
+      paint = Paint();
+      paint.color = boxColor;
+      paint.style = PaintingStyle.fill;
+    }
+    if (borderColor != null) {
+      border = Paint();
+      border.color = borderColor;
+      border.style = PaintingStyle.stroke;
+      border.strokeWidth = borderThickness;
+    }
+
+    tp.textAlign = textAlign;
+    tp.textScaleFactor = scale;
+    tp.text = textSpan;  // text to draw
+    tp.layout(minWidth: position.width, maxWidth: position.width, );
+
+    // see if box is too short
+    if (tp.height > position.height) {
+      position = Rect.fromLTRB(position.left, position.top, position.right, position.top + tp.height);
+    }
+
+  }
+
+  void resize(Size size) {
+  }
+
+  void render(Canvas c) {
+    // draw the box, the border, and then the text on top
+    if (paint != null) c.drawRect(position, paint);
+    if (border != null) c.drawRect(position, border);
+    tp.paint(c, Offset(position.left, position.top));
+  }
+
+  void update(double t) {
+  }
+
+  bool destroy() => lives <= 0;
+
+}
--- lib/flutterball_game.dart
+++ lib/flutterball_game.dart
@@ -2,7 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flame/game.dart';
-import 'package:flutter_ball/components/block.dart';
+import 'package:flutter_ball/components/text.dart';
 
 class FlutterballGame extends BaseGame {
   // instance variables
@@ -15,11 +15,34 @@ class FlutterballGame extends BaseGame {
 
   // make a new game
   FlutterballGame() {
-    // make a new block game component
-    var block = Block(this, position: Rect.fromLTWH(100, 200, 50, 50));
+    // add simple text block to game
+    Rect pos = Rect.fromLTRB(100, 100, 200, 200);
+    TextStyle style = TextStyle(color: Colors.black, fontSize: 10, );
+    TextSpan span = TextSpan(text: "By default, text is centered.  Notice how the box grows to hold the text.", style: style);
+    var text = TextDraw(pos, span, borderColor: Colors.green, boxColor: Colors.lightBlueAccent);
+    add(text);
 
-    // tell the game about this component
-    add(block);
+    // add complex text to game
+    pos = Rect.fromLTRB(  0, 500, 500, 600);
+    span = TextSpan(
+      text: 'Can you ',
+      style: TextStyle(color: Colors.yellow),
+      children: <InlineSpan>[
+        TextSpan(
+          text: 'find the',
+          style: TextStyle(
+            color: Colors.green,
+            decoration: TextDecoration.underline,
+            decorationStyle: TextDecorationStyle.wavy,
+          ),
+        ),
+        TextSpan(
+          text: ' secret?\n',
+        ),
+      ],
+    );
+    text = TextDraw(pos, span, textAlign: TextAlign.left);
+    add(text);
   }
 
   // gesture handlers
