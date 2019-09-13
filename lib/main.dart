2b2cc8692ead1a772fd40e6aca6df9ddb55d010a
shawn <shawn@gordhamer.com>
2019-09-12 19:59:55 -0500

    add slide pdfs

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_ball/flutterball_game.dart';
-
-void main() async {
-  print("starting main");
-  FlutterballGame game = FlutterballGame();
-  runApp(game.widget);
-}
-
